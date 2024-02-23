7a6c7f9dca64a1442c60a534c937b77ba975b376
Nikita <nerdyaus21@gmail.com>
2024-02-23 19:33:12 +0300

    added components

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-
-
-
-
 export default function Home() {
   return (
-        <main>just try to do this! \ntryrtttt</main>
-    )
+    <main>
+      <p>just try to do this! \ntryrtttt</p>
+      <p>second p</p>
+    </main>
+  );
 }
