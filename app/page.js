59e18395960aed6b09a4deb53f748cbb8b300a91
Nikita <nerdyaus21@gmail.com>
2024-02-23 20:23:19 +0300

    added home components

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Banner } from "./components/Banner/Banner.jsx";
+import { Promo } from "./components/Promo/Promo.jsx";
+import { NewCardsList } from "./components/CardsList/NewCardsList.jsx";
+import { PopularCardsList } from "./components/CardsList/PopularCardsList.jsx";
 export default function Home() {
   return (
-    <main>
-      <p>just try to do this! \ntryrtttt</p>
-      <p>second p</p>
+    <main className="main">
+      <Banner />
+      <Promo />
+      <NewCardsList />
+      <PopularCardsList />
     </main>
   );
 }
