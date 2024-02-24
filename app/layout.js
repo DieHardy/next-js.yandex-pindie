8fb40d1da845b76772dab6ae991885f040822ccb
Nikita <nerdyaus21@gmail.com>
2024-02-24 19:56:11 +0300

    test module styles

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Header } from "./components/Header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
+import { Card } from "./components/Card/Card.jsx";
 export const metadata = {
   title: "Pindie - мир видеоигр",
   description: "Pindie - как rustore, но лучше!",
@@ -11,6 +12,7 @@ export default function RootLayout() {
     <html lang="ru">
       <body>
         <Header />
+        <Card />
         <Home />
         <Footer />
       </body>
